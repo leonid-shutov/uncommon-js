6db2f47ae16cfbc1bbfe35faf0c14a9256b8b1eb
leonid-shutov <leonid.shutov.main@gmail.com>
2025-05-31 19:19:56 +0200

    export

--- loader.js
+++ loader.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const loader = require('./lib/loader.js');
+const rest = require('./lib/rest.js');
 const errors = require('./lib/errors.js');
 
-module.exports = { ...loader, ...errors };
+module.exports = { ...loader, ...rest, ...errors };
