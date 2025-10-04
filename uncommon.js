3052cea5cb4fb58dee6e896b0c0713aa39f3bcfe
leonid-shutov <leonid.shutov.main@gmail.com>
2025-10-04 10:31:39 +0200

    improvements

--- uncommon.js
+++ uncommon.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const loader = require('./lib/loader.js');
+const application = require('./lib/application.js');
 const rest = require('./lib/rest.js');
 const errors = require('./lib/errors.js');
 
-module.exports = { ...loader, ...rest, ...errors };
+module.exports = { ...loader, ...application, ...rest, ...errors };
